8be4ccf0893745fecccdc29aaaeaca27dfbe87c5
CharizardBrillante <106075512+CharizardBrillante@users.noreply.github.com>
2022-06-07 17:58:43 +0200

    Add files via upload

---
+++ Esercizio 2/js/script.js
@@ -0,0 +1,25 @@
+function randomDate() {
+    let now = new Date();
+    return new Date(now.getTime() + Math.random() * (new Date(2080,0,1).getTime() - now.getTime()));
+}
+
+let deathDate = randomDate();  
+
+function countDown(deathDate) {
+    
+    let now = new Date().getTime();
+    let distance = deathDate.getTime() - now;
+    let years = Math.floor(distance / (1000 * 60 * 60 * 24 * 30 * 12));
+    let months = Math.floor((distance % (1000 * 60 * 60 * 24 * 30 * 12)) / (1000 * 60 * 60 * 24 * 30)) ;
+    let days = Math.floor((distance % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("timer").innerHTML = `<p>${years}y ${months}m ${days}d ${hours}h ${minutes}m ${seconds}s</p>
+    <img src="img/grave.svg">`;
+}
+
+
+
+
+
